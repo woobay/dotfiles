68d5931b58123e07fba97f90d89da2a81255c007
woobay <gl.gabriellafrance@gmail.com>
2024-05-01 19:35:26 -0400

    added .config

--- setup.sh
+++ setup.sh
@@ -7,6 +7,16 @@ DOTFILES=(
 	".alacritty.toml"
 	".tmux.conf"
 	".gitconfig"
+	".config/nvim/init.lua"
+	".config/nvim/lua/custom/plugins/init.lua"
+	".config/nvim/lua/custom/plugins/lazy-lock.json"
+	".config/nvim/lua/kickstart/plugins/autopairs.lua"
+	".config/nvim/lua/kickstart/plugins/debug.lua"
+	".config/nvim/lua/kickstart/plugins/gitsigns.lua"
+	".config/nvim/lua/kickstart/plugins/indent_line.lua"
+	".config/nvim/lua/kickstart/plugins/lint.lua"
+	".config/nvim/lua/kickstart/plugins/neo-tree.lua"
+	".config/nvim/lua/kickstart/health.lua"
 )
 
 for dotfile in "${DOTFILES[@]}";do
