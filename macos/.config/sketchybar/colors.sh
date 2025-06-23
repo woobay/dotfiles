a0085ba3ecf5cbc53feab17cf22bb53188b60630
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2025-06-23 11:02:21 -0400

    feat: updated config for macos

--- macos/.config/sketchybar/colors.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export WHITE=0xffcdd6f4
-
-export BAR_COLOR=0x0011111b      # Transparent base
-export ITEM_BG_COLOR=0x40313244  # Semi-transparent surface0
-export ACCENT_COLOR=0xff89b4fa   # Catppuccin blue
