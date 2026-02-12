3ccf146207d5873ea4952cd7e05f89064d4dd80e
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2026-02-11 20:34:30 -0500

    chore: update

--- common/.config/nvim/lua/config/plugins/lsp.lua
+++ common/.config/nvim/lua/config/plugins/lsp.lua
@@ -5,62 +5,70 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
-
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
       vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
       vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
       vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
+      
       vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-	callback = function(ev)
-	  -- Enable completion triggered by <c-x><c-o>
-	  vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-	  -- Buffer local mappings.
-	  -- See `:help vim.lsp.*` for documentation on any of the below functions
-	  local opts = { buffer = ev.buf }
-	  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-	  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-	  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-	  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-	  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-	  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-	  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-	  vim.keymap.set('n', '<space>wl', function()
-	    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	  end, opts)
-	  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-	  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-	  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-	  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-	  vim.keymap.set('n', '<space>f', function()
-	    vim.lsp.buf.format { async = true }
-	  end, opts)
-	end,
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
       })
-
-
-      require('lspconfig').helm_ls.setup {
-	capabilities = capabilities,
-	settings = {
-	  ['helm-ls'] = {
-	    yamlls = {
-	      path = 'yaml-language-server',
-	    }
-	  }
-	}
-      }
-      require('lspconfig').yamlls.setup {}
-
-      require('lspconfig').terraformls.setup { capabilities = capabilities }
+      
+      -- New API for Neovim 0.11+
+      vim.lsp.config('helm_ls', {
+        capabilities = capabilities,
+        cmd = { 'helm_ls', 'serve' },
+        filetypes = { 'helm' },
+        root_markers = { 'Chart.yaml' },
+        settings = {
+          ['helm-ls'] = {
+            yamlls = {
+              path = 'yaml-language-server',
+            }
+          }
+        }
+      })
+      
+      vim.lsp.config('yamlls', {
+        capabilities = capabilities,
+        cmd = { 'yaml-language-server', '--stdio' },
+        filetypes = { 'yaml', 'yaml.docker-compose' },
+      })
+      
+      vim.lsp.config('terraformls', {
+        capabilities = capabilities,
+        cmd = { 'terraform-ls', 'serve' },
+        filetypes = { 'terraform', 'tf', 'terraform-vars' },
+      })
+      
       vim.api.nvim_create_autocmd({"BufWritePre"}, {
-	pattern = {'*.t', '*.tfvars'},
-	callback = function()
-	  vim.lsp.buf.format()
-	end
-
+        pattern = {'*.tf', '*.tfvars'},
+        callback = function()
+          vim.lsp.buf.format()
+        end
       })
     end
   }
---
+++ common/.config/nvim/lua/config/plugins/obsidian.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+	{
+	  name = "personal",
+	  path = "~/workspace/github.com/woobay/notes",
+	},
+      },
+    },
+  }
+}
+
